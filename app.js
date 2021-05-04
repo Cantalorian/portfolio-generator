186d383b0c564246f4273da365ffa169fd527a00
Chewie <chewie1985@gmail.com>
2021-05-03 20:55:08 -0600

    multi-page link

--- app.js
+++ app.js
@@ -1,14 +1,7 @@
 const inquirer = require("inquirer");
-// const fs = require("fs");
-// const generatePage = require("./src/page-template.js");
+const fs = require("fs");
+const generatePage = require("./src/page-template.js");
 
-// const pageHTML = generatePage(userName, githubName);
-
-// fs.writeFile("./index.html", pageHTML, err => {
-//   if (err) throw err;
-
-//   console.log("Portfolio complete! Check out index.html to see the output!")
-// });
 
 const promptUser = () => {
   return inquirer.prompt([
@@ -62,26 +55,26 @@ const promptUser = () => {
 const promptProject = portfolioData => {
   if (!portfolioData.projects) {
   portfolioData.projects = [];
-  }
-  console.log(``);
-  return inquirer.prompt([
-    {
-      type: "input",
-      name: "name",
-      message: "What is the name of your project? (Required)",
-      validate: projectName => {
-        if (projectName) {
-          return true;
-        } else {
-          console.log("Please enter your project's name!");
-          return false;
-        }
+}
+console.log(``);
+return inquirer.prompt([
+  {
+    type: "input",
+    name: "name",
+    message: "What is the name of your project? (Required)",
+    validate: projectName => {
+      if (projectName) {
+        return true;
+      } else {
+        console.log("Please enter your project's name!");
+        return false;
       }
-    },
-    {
-      type: "input",
-      name: "description",
-      message: "Provide a description of the project (Required)",
+    }
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Provide a description of the project (Required)",
       validate: projectDescription => {
         if (projectDescription) {
           return true;
@@ -134,7 +127,13 @@ const promptProject = portfolioData => {
 };
 
 promptUser()
-  .then(promptProject)
-  .then(portfolioData => {
-    console.log(portfolioData);
-  });
+.then(promptProject)
+.then(portfolioData => {
+  const pageHTML = generatePage(portfolioData);
+  
+  // fs.writeFile("./index.html", pageHTML, err => {
+  //   if (err) throw err;
+  
+  //   console.log("Portfolio complete! Check out index.html to see the output!")
+  // });
+});
